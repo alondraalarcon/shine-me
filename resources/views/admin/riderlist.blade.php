000841eb267ab4733ed5d780d35f7af25f4e4dc5
Ma. Alondra Alarcon <48515832+alondraalarcon@users.noreply.github.com>
2021-09-15 09:43:36 +0800

    Rider List UI

--- resources/views/admin/riderlist.blade.php
+++ resources/views/admin/riderlist.blade.php
@@ -1,35 +1,77 @@
 @extends('admin/layout/template')
 
-@section('title', 'Rider List')
+@section('title', 'Carwash Provider')
 @section('content')
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-body">
-                    <table id="example" class="table table-striped dataTable no-footer dtr-inline" style="width: 100%;" role="grid" aria-describedby="datatables-reponsive_info">
-                        <thead>
-                            <tr role="row">
-                                <th> Name</th>                                
-                                <th> Address</th>                                
-                                <th> Phone</th>                                
-                                <th> Email </th>                                
-                                <th> Action</th>                                
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Airi Satou</td>
-                                <td>Quezon City</td>
-                                <td>+639999393455</td>
-                                <td>Test</td>
-                                <td></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>     
+    <div class="tab">
+        <ul class="nav nav-tabs" role="tablist">
+            <li class="nav-item"><a class="nav-link" href="#Request" data-bs-toggle="tab" 
+            role="tab" aria-selected="false">Request</a></li>
+            <li class="nav-item"><a class="nav-link active" href="#Approved" data-bs-toggle="tab" 
+            role="tab" aria-selected="true">Approved</a></li>
+        </ul>
+
+        <div class="tab-content active">
+            <div class="tab-pane" id="Request" role="tabpanel">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <table id="example" class="table table-striped dataTable no-footer dtr-inline" style="width: 100%;" role="grid" aria-describedby="datatables-reponsive_info">
+                                    <thead>
+                                        <tr role="row">
+                                            <th> Name</th>                                
+                                            <th> Address</th>                                
+                                            <th> Phone</th>                                
+                                            <th> Email </th>                                
+                                            <th> Action</th>                                
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Airi Satou</td>
+                                            <td>Quezon City</td>
+                                            <td>+639999393455</td>
+                                            <td>Test</td>
+                                            <td></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>     
+                        </div>
+                    </div>
+                </div>  <!-- END OF DIV ROW -->
             </div>
+            <div class="tab-pane" id="Approved" role="tabpanel">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <table id="example" class="table table-striped dataTable no-footer dtr-inline" style="width: 100%;" role="grid" aria-describedby="datatables-reponsive_info">
+                                    <thead>
+                                        <tr role="row">
+                                            <th> Name</th>                                
+                                            <th> Address</th>                                
+                                            <th> Phone</th>                                
+                                            <th> Email </th>                                
+                                            <th> Action</th>                                
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Airi Satou</td>
+                                            <td>Quezon City</td>
+                                            <td>+639999393455</td>
+                                            <td>Test</td>
+                                            <td></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>     
+                        </div>
+                    </div>
+                </div>  <!-- END OF DIV ROW -->
+            </div>  <!-- END OF DIV Approved -->
         </div>
     </div>
 
-    
 @stop
